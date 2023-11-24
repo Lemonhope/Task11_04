a8577709bbad9358da9b6c8797257eb36eab73f1
Kseniia <betrezen145@gmail.com>
2023-11-24 21:24:27 +0200

    Update Main.java

--- Task11_04/src/main/java/com/javapro/Main.java
+++ Task11_04/src/main/java/com/javapro/Main.java
@@ -3,17 +3,25 @@ package com.javapro;
 public class Main {
     public static void main(String[] args) {
         try {
-            MyThread thread1=new MyThread("First Thread");
-            thread1.setPriority(Thread.MAX_PRIORITY);
-            MyThread thread2=new MyThread("Second Thread");
-            thread2.setPriority(9);
-            MyThread thread3=new MyThread("Third Thread");
-            thread3.setPriority(8);
-            thread1.start();
-            thread2.start();
-            thread3.start();
 
-            Thread.sleep(1000);
+            MyThread thread1=new MyThread("First Thread");
+            MyThread thread2=new MyThread("Second Thread");
+            MyThread thread3=new MyThread("Third Thread");
+
+            thread1.start();
+            thread1.join(200);
+            thread2.start();
+            thread1.join();
+
+            thread3.start();
+            try {
+                thread1.join();
+                thread2.join();
+                thread3.join();
+            } catch (Exception e) {
+                System.out.println(e);;
+            }
+
             System.out.println("All threads finished!");
         }catch (Exception e){
             System.out.println(e);
